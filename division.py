9d5c72f4c99bde71b029bee41d65de705adb3042
AlimbekovE <34837541+AlimbekovE@users.noreply.github.com>
2017-12-29 14:54:41 +0600

    Add files via upload

---
+++ division.py
@@ -0,0 +1,4 @@
+num_1 = input()
+num_2 = input()
+print(int (num_1) // int (num_2))
+print(float (num_1) / float (num_2))
---
+++ print Function.py
@@ -0,0 +1,7 @@
+num = int(input())
+i = 1
+res = ""
+while i <= num:
+	res += str(i)
+	i += 1
+print(res)
